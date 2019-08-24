d0c293d2842a59f4f86e3f228af07843cca2c72b
mayur <mayurbabariya45@gmail.com>
2019-08-24 13:57:43 +0530

    msg service

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@ const clock = require('./clock/clock.service.js');
 const box = require('./box/box.service.js');
 const prodtask = require('./prodtask/prodtask.service.js');
 const states = require('./states/states.service.js');
+const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(login);
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.configure(box);
   app.configure(prodtask);
   app.configure(states);
+  app.configure(messages);
 };
